13432af8e41a60069a1f466cdc38247ed9917291
DeleteZN <32798045+DeleteZN@users.noreply.github.com>
2018-11-10 14:20:26 +0800

    Add files via upload

---
+++ MoocHomework.js
@@ -0,0 +1,17 @@
+document.body.appendChild(document.createElement('script')).src='//code.jquery.com/jquery-1.9.1.min.js';
+setTimeout(function(){
+    $(".s label:last-child").each(
+        function(){
+            $(this).click();
+        });
+    $(".j-textarea").each(
+        function(){
+            $(this).val("Good Job."); //这里是点评的内容
+        });
+},1000)
+setTimeout(function(){
+    document.getElementsByClassName('j-submitbtn')[0].click();
+},1000)
+setTimeout(function(){
+    document.getElementsByClassName('j-gotonext')[0].click();
+},1000)
